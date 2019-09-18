36ce5d6b12eb9d4648e2e41c07174c415054d8c1
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2019-09-18 12:23:07 -0700

    Started working on comments page

---
+++ BobzBurger/Pages/RateUs.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BobzBurger.Pages
+{
+    public class RateUsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
