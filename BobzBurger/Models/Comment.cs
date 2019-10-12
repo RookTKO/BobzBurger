30987beb6b4e40ebe486958961522d59e85f5105
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2019-10-12 16:14:58 -0700

    Started models for comment and user photo

---
+++ BobzBurger/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+// TODO : Remove or/redo?
+namespace BobzBurger.Models
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public int PhotoID { get; set; }
+        public string UserFirstName { get; set; }
+        [Required]
+        [StringLength(250)]
+        public string Subject { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+        public virtual Photo Photo { get; set; }
+    }
+}
