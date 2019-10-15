d1eb8b756362e52b97c7b30971d0dd345467c4f2
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2019-10-15 00:17:50 -0700

    ICommentService interface created

---
+++ BobzBurger/Models/ICommentService.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace BobzBurger.Models
+{
+    public interface ICommentService
+    {
+        Task DeleteAsync(long id);
+        Recipe Find(long id);
+        Task<Recipe> FindAsync(long id);
+        IQueryable<Recipe> GetAll(int? count = null, int? page = null);
+        Task<Recipe[]> GetAllAsync(int? count = null, int? page = null);
+        Task SaveAsync(Recipe recipe);
+    }
+}
