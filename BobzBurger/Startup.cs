21f2c242aa45c17662de3efb079431194c6ad51b
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2019-10-16 13:15:30 -0700

    Working on fixing routing for recipes

--- BobzBurger/Startup.cs
+++ BobzBurger/Startup.cs
@@ -25,6 +25,8 @@ namespace BobzBurger
                 options.Conventions.AllowAnonymousToPage("/Account/Login");
             }); //dependency injection
             services.AddTransient<IRecipesService, RecipesService>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +40,7 @@ namespace BobzBurger
             app.UseAuthentication();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+
         }
     }
 }
